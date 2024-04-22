813bc1b20415711de4030698073c5865a58f42f3
RodrigoCardinal <rodrigo.cardinal@correo.ucu.edu.uy>
2024-04-22 16:24:28 -0300

    Test Terminados

--- src/Library/DateFormatter.cs
+++ src/Library/DateFormatter.cs
@@ -1,4 +1,4 @@
-﻿namespace Ucu.Poo.TestDateFormat;
+﻿namespace TestDateFormat;
 
 /// <summary>
 /// Esta clase implementa la funcionalidad de cambiar el formato de una fecha.
@@ -15,6 +15,11 @@ public class DateFormatter
     /// <returns>La fecha convertida al formato "yyyy-mm-dd".</returns>
     public static string ChangeFormat(string date)
     {
+
+        if (date.Length !=10)
+        {
+            return "";
+        }
         return date.Substring(6) + "-" + date.Substring(3, 2) + "-" + date.Substring(0, 2);
     }
 }
